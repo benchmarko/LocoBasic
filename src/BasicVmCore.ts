9b86ddd02d497428741847f1b25a2a4d48536840
Benchmarko <benchmarko@users.noreply.github.com>
2025-04-17 00:46:06 +0200

    put RSX in map; check RSX argument types

--- src/BasicVmCore.ts
+++ src/BasicVmCore.ts
@@ -1,5 +1,10 @@
 import type { IVmAdmin, TimerMapType } from "./Interfaces";
 
+type RsxMapType = Record<string, {
+    argTypes: string[];
+    fn: (args: (number | string)[]) => void;
+}>;
+
 const strokeWidthForMode: number[] = [4, 2, 1, 1];
 
 export class BasicVmCore implements IVmAdmin {
@@ -246,22 +251,8 @@ export class BasicVmCore implements IVmAdmin {
         }
     }
 
-    private static getRsxNumArgs(cmd: string, args: (number | string)[], count: number): number[] {
-        if (args.length !== count) {
-            throw new Error(`|${cmd.toUpperCase()}: Wrong number of arguments: ${args.length} <> ${count}`);
-        }
-        return args.map((p) => typeof p === "number" ? Math.round(p) : 0);
-    }
-
-    private static getRsxStrArgs(cmd: string, args: (number | string)[], count: number): string[] {
-        if (args.length !== count) {
-            throw new Error(`|${cmd.toUpperCase()}: Wrong number of arguments: ${args.length} <> ${count}`);
-        }
-        return args.map((p) => typeof p === "string" ? p : String(p));
-    }
-
-    private rsxCircle(cmd: string, args: (number | string)[]) {
-        const [x, y, radius] = BasicVmCore.getRsxNumArgs(cmd, args, 3);
+    private rsxCircle(args: (number | string)[]) {
+        const [x, y, radius] = args.map((p) => Math.round(p as number));
         let strokeStr = "";
         if (this.currGraphicsPen >= 0) {
             const color = this.cpcColors[this.colorsForPens[this.currGraphicsPen]];
@@ -274,11 +265,7 @@ export class BasicVmCore implements IVmAdmin {
 
     // returns a date string in the format "ww DD MM YY" with ww=day of week
     // see https://www.cpcwiki.eu/imgs/b/b4/DXS_RTC_-_User_Manual.pdf
-    private rsxDate(cmd: string, args: (number | string)[]) {
-        const [str] = BasicVmCore.getRsxStrArgs(cmd, args, 1);
-        if (str.length < 11) {
-            throw new Error(`|${cmd.toUpperCase()}: String too short!`);
-        }
+    private rsxDate(args: (number | string)[]) {
         const date = new Date();
         // Get the day of the week (0-6) and convert to 1-7
         const dayOfWeek = (date.getDay() + 1) % 7;
@@ -289,9 +276,9 @@ export class BasicVmCore implements IVmAdmin {
         args[0] = dateStr;
     }
 
-    private rsxRect(cmd: string, args: (number | string)[]) {
+    private rsxRect(args: (number | string)[]) {
         // Extract and round arguments
-        const [x1, y1, x2, y2] =  BasicVmCore.getRsxNumArgs(cmd, args, 4);
+        const [x1, y1, x2, y2] = args.map((p) => Math.round(p as number));
     
         // Calculate position and dimensions
         const x = Math.min(x1, x2);
@@ -311,11 +298,7 @@ export class BasicVmCore implements IVmAdmin {
 
     // returns a time string in the format "HH MM SS"
     // see https://www.cpcwiki.eu/imgs/b/b4/DXS_RTC_-_User_Manual.pdf
-    private rsxTime(cmd: string, args: (number | string)[]) {
-        const [str] = BasicVmCore.getRsxStrArgs(cmd, args, 1);
-        if (str.length < 8) {
-            throw new Error(`|${cmd.toUpperCase()}: String too short!`);
-        }
+    private rsxTime(args: (number | string)[]) {
         const date = new Date();
         const hours = date.getHours();
         const minutes = date.getMinutes();
@@ -324,23 +307,45 @@ export class BasicVmCore implements IVmAdmin {
         args[0] = timeStr;
     }
 
-    public rsx(cmd: string, args: (number | string)[]): (number | string)[] {
-         switch (cmd) {
-            case "circle":
-                this.rsxCircle(cmd, args);
-                break;
-            case "date":
-                this.rsxDate(cmd, args);
-                break;
-            case "rect":
-                this.rsxRect(cmd, args);
-                break;
-            case "time":
-                this.rsxTime(cmd, args);
-                break;
-            default:
-                throw new Error(`Unknown RSX command: |${cmd}`);
+    private rsxMap: RsxMapType = {
+        circle: {
+            argTypes: ["number", "number", "number"],
+            fn: this.rsxCircle.bind(this)
+        },
+        date: {
+            argTypes: ["string"],
+            fn: this.rsxDate.bind(this)
+        },
+        rect: {
+            argTypes: ["number", "number", "number", "number"],
+            fn: this.rsxRect.bind(this)
+        },
+        time: {
+            argTypes: ["string"],
+            fn: this.rsxTime.bind(this)
         }
+    }
+
+    public rsx(cmd: string, args: (number | string)[]): (number | string)[] {
+        if (!this.rsxMap[cmd]) {
+            throw new Error(`Unknown RSX command: |${cmd.toUpperCase()}`);
+        }
+        const rsxInfo = this.rsxMap[cmd];
+        const expectedArgs = rsxInfo.argTypes.length;
+
+        if (args.length !== expectedArgs) {
+            throw new Error(`|${cmd.toUpperCase()}: Wrong number of arguments: ${args.length} <> ${expectedArgs}`);
+        }
+
+        for (let i = 0; i < args.length; i += 1) {
+            const expectedType = rsxInfo.argTypes[i];
+            const arg = args[i];
+            if (typeof arg !== expectedType) {
+                throw new Error(`|${cmd.toUpperCase()}: Wrong argument type (pos ${i}): ${typeof arg}`);
+            }
+        }
+
+        rsxInfo.fn(args);
         return args;
     }
 
