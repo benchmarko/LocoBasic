845fb6bbe8ba19071a0156ea069de6371b5f6eaa
Benchmarko <benchmarko@users.noreply.github.com>
2025-04-30 02:05:36 +0200

    example yinyang with filled areas

--- dist/examples/yinyang.js
+++ dist/examples/yinyang.js
@@ -6,34 +6,49 @@ cpcBasic.addItem("", `
 REM yinyang - Yin and yang
 REM https://rosettacode.org/wiki/Yin_and_yang#Locomotive_Basic
 REM GNU FDL 1.2 (https://www.gnu.org/licenses/fdl-1.2.html)
-REM modifications: removed DEFINT, BORDER, FILL, wait loop
-REM TODO: fill
-MODE 2:DEG:'defint a-z
-INK 0,26:INK 1,0 ':border 26
-xp=320:yp=200:size=150:GOSUB 100
-xp=550:yp=350:size=40:GOSUB 100
-'WHILE INKEY$="":WEND
+REM modifications: removed DEFINT, BORDER, FILL, wait loop; draw horizontal lines to fill
+m=2
+MODE m:DEG
+INK 0,26:INK 1,0
+lbasic=&8000>0: 'fast hack to detect LocoBasic
+IF lbasic OR m=3 THEN yd=1 ELSE yd=2
+xp=320:yp=200:size=150:GOSUB 100: 'large yinyang
+xp=550:yp=350:size=40:GOSUB 100: 'small yinyang
 END
 '
-100 cx=xp:cy=yp:cr=size:GOSUB 1000
-cy=yp+size/2:cr=size/8:GOSUB 1000
-cr=size/2:half=0:GOSUB 2000
-cy=yp-size/2:cr=size/8:GOSUB 1000
-cr=size/2:half=1:GOSUB 2000
-MOVE xp, yp+size/2 ':fill 1
-MOVE xp+size/2, yp ':fill 1
+100 ORIGIN xp,yp
+cr=size:cr2=size/2
+GOSUB 1000: 'filled right half of yinyang
+GOSUB 1500: 'continue with left half circle
+ORIGIN xp,yp+cr2
+cr=size/8
+GOSUB 2000: 'upper small circle
+GRAPHICS PEN 0
+ORIGIN xp,yp-cr2
+GOSUB 2000: 'lower filled small circle (background color)
+GRAPHICS PEN 1
 RETURN
 '
-1000 PLOT cx,cy+cr
-FOR i=0 TO 360 STEP 10
-DRAW cx+cr*SIN(i),cy+cr*COS(i)
+' Draw filled right half of Yin-Yang
+1000 FOR y=0 TO cr-1 STEP yd
+  dx=SQR(cr^2-y^2)
+  dx2=SQR(cr2^2-(y-cr2)^2)
+  MOVE -dx2,-y: DRAW dx,-y 'lower half
+  MOVE dx2,y: DRAW dx,y 'upper half
 NEXT
 RETURN
 '
-2000 p=180*half
-PLOT cx+cr*SIN(p),cy+cr*COS(p)
-FOR i=p TO p+180 STEP 10
-DRAW cx+cr*SIN(i),cy+cr*COS(i)
+' Draw left half circle (no MOVE)
+1500 FOR i=360 TO 180 STEP -10
+  DRAW cr*SIN(i),cr*COS(i)
+NEXT
+RETURN
+'
+' Draw a filled circle
+2000 FOR y=0 TO cr STEP yd
+  dx=SQR(cr^2-y^2)
+  MOVE -dx,-y: DRAW dx,-y 'lower half
+  MOVE -dx,y: DRAW dx,y 'upper half
 NEXT
 RETURN
 `);
