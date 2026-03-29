253816e379d45bf2a250128f8bab2c41e4095349
benchmarko <benchmarko@users.noreply.github.com>
2026-03-29 09:34:39 +0000

    deploy: b4788055abb7549d3a6e195fc783a4016b14388b

--- examples/rayclip.js
+++ examples/rayclip.js
@@ -5,47 +5,37 @@
 cpcBasic.addItem("", `
 REM rayclip - Ray Eclipse
 REM (c) Alan Scully
-REM https://www.cpcwiki.eu/forum/programming/basic-files/ (BASIC02.DSK)
-REM Modifications: redraw for every animation step
+REM https://www.cpcwiki.eu/forum/programming/basic-files/#msg192678 (BASIC02.DSK)
+REM Modifications: redraw for every animation step; simplified for small beams
 REM
 REM RAY ECLIPSE: ALAN SCULLY for SCULL PD LIBRARY 119 Laurel Drive East Kilbride G75 9JG
 DEG
 MODE 0
-INK 14,0:INK 0,0
+INK 0,0
 GOSUB 500
+ORIGIN 320,50
 '
-s=50
-WHILE s>0
-  D=d MOD 13+1
-  FOR E=1 TO 13
-    INK E,COL((D+E) MOD 13)
+s=0
+WHILE s<100
+  FOR e=1 TO 13
+    INK e,col((s+e) MOD 13)
   NEXT
   CLS
-  GOSUB 400
-  t=TIME+12:WHILE TIME<t:FRAME:WEND
-  s=s-1
+  FOR m=60 TO 300 STEP 1
+    a=SIN(m):b=-COS(m)
+    MOVE 100*a,100*b
+    DRAW 500*a,500*b,m MOD 13+1
+  NEXT m
+  t=TIME+15:WHILE TIME<t:FRAME:WEND
+  s=s+1
 WEND
 END
 '
-400 ORIGIN 320,50
-FOR m=60 TO 300 STEP 0.5
-  a=SIN(m):b=-COS(m)
-  MOVE 100*a,100*b
-  x=XPOS:y=YPOS
-  c=m MOD 13+1
-  DRAW 600*a,600*b,c
-  x2=XPOS:y2=YPOS
-  MOVER 4,0:DRAW x+4,y
-  MOVE x,y-2:DRAW x2,y2-2
-NEXT m
-RETURN
-'
 ' SET UP COLOURS
-500 DIM col(14)
-FOR a=0 TO 14
+500 DIM col(12)
+FOR a=0 TO 12
   READ col(a)
-  INK a+1,col(a)
 NEXT
 RETURN
-DATA 3,9,11,15,21,24,23,22,19,12,10,4,1,7,8
+DATA 3,9,11,15,21,24,23,22,19,12,10,4,1
 `);
